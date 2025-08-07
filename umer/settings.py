141a7305490e1036a27fcd873ee41e3e41d271d2
your_username <your_email>
2025-08-07 12:20:34 +0500

    changed the allowed host to all

--- umer/settings.py
+++ umer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yy+4y6$&=1u7*5_+8&%)^2qhdl1o$%myrmbtx+b2nfycmcb8x#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
